6e0cc456c9be58f9e5191da5cf28bda6cf0929c1
Jorge Casariego <jorgekasa@gmail.com>
2016-07-08 16:13:28 -0400

    Angular 2 - Starter project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>My app componente</h1>'
+})
+
+// Para que funcione los pasos son:
+// 1. npm install
+// 2. npm run build
+
+export class AppComponent{}
